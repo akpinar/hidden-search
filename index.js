13bd6486b3863e994adcc4c5d8e13664b7f802f2
akpiinar <senaa.akpinarr@gmail.com>
2021-01-25 17:24:15 +0300

    yorum satırları temizlendi

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 var button = document.getElementById("button");
-// var div = document.getElementById("div")
-
-// button.addEventListener("click", function () {
-//     if (div.style.display === "none") {
-//         div.style.display = "block"
-//     }
-//     else {
-//         div.style.display = "none"
-//     }
-// })
 
 $("button").click(function(){
     $("input").toggle();
